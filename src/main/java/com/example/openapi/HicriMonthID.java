22d5cd1a9f45f52b536842dd4385717ac7ea6588
mbK <burakkaraoglan55@gmail.com>
2019-11-15 18:02:15 +0300

    refc: add excel read failing resolve it

--- src/main/java/com/example/openapi/HicriMonthID.java
+++ src/main/java/com/example/openapi/HicriMonthID.java
@@ -1,27 +1,32 @@
 package com.example.openapi;
 
 public enum HicriMonthID {
-    MUHARREM("1"),
-    SAFER("2"),
-    RABIULEVVEL("3"),
-    RABIULAHIR("4"),
-    CEMAZIYELEVVEL("5"),
-    CEMAZIYELAHIR("6"),
-    RECEP("7"),
-    SABAN("8"),
-    RAMAZAN("9"),
-    SEVVAL("10"),
-    ZILKADE("11"),
-    ZILHICCE("12");
+    MUHARREM("1", "M"),
+    SAFER("2", "S"),
+    RABIULEVVEL("3", "RA"),
+    RABIULAHIR("4", "R"),
+    CEMAZIYELEVVEL("5", "CA"),
+    CEMAZIYELAHIR("6", "C"),
+    RECEP("7", "B"),
+    SABAN("8", "Ş"),
+    RAMAZAN("9", "N"),
+    SEVVAL("10", "L"),
+    ZILKADE("11", "ZA"),
+    ZILHICCE("12", "Z");
 
+    private String value;
     private String code;
 
-    HicriMonthID(String code) {
+    HicriMonthID(String value, String code) {
+        this.value = value;
         this.code = code;
+    }
+
+    public String getValue() {
+        return value;
     }
 
     public String getCode() {
         return code;
     }
-
 }
--- src/test/java/com/example/openapi/SeleniumTest.java
+++ src/test/java/com/example/openapi/SeleniumTest.java
@@ -1,5 +1,10 @@
 package com.example.openapi;
 
+import org.apache.poi.openxml4j.util.ZipSecureFile;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +17,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SeleniumTest {
@@ -19,10 +27,13 @@ public class SeleniumTest {
     @Value("${geckodriver.path}")
     private String driverPath;
 
-    public static final String HICRI_DAY = "25";
-    public static final String HICRI_YEAR = "1219";
+    @Value("${sheets.path}")
+    private String SHEETS_PATH;
+
+    private String HICRI_DAY = "25";
+    private String HICRI_YEAR = "1219";
     public static final String CALENDAR_CODE = CalendarOption.HICRI.getCode();
-    public static final String HICRI_MONTH_CODE = HicriMonthID.ZILHICCE.getCode();
+    private String HICRI_MONTH_CODE = HicriMonthID.ZILHICCE.getCode();
 
     private WebDriver driver;
 
@@ -33,7 +44,11 @@ public class SeleniumTest {
     }
 
     @Test
-    public void testTest() throws Exception {
+    public void test() {
+        readExcel();
+    }
+
+    private void callWebsite() {
         driver.get("http://193.255.138.133/takvim.asp");
 
         WebElement convertButton = driver.findElement(By.xpath("//input[@value='ÇEVİR']"));
@@ -62,6 +77,51 @@ public class SeleniumTest {
         System.out.println("------------------------------");
         System.out.println(toMiladiDay + " " + toMiladiMonth + " " + toMiladiYear + " / " + toMiladiDayName);
         System.out.println("------------------------------");
+    }
 
+    private void readExcel() {
+        try {
+            FileInputStream file = new FileInputStream(SHEETS_PATH);
+            ZipSecureFile.setMinInflateRatio(-1.0d);
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            // Retrieving the number of sheets in the Workbook
+            System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        /*
+           =============================================================
+           Iterating over all the sheets in the workbook (Multiple ways)
+           =============================================================
+        */
+
+            // 1. You can obtain a sheetIterator and iterate over it
+            System.out.println("Retrieving Sheets using Iterator");
+
+            Sheet sheet  = workbook.getSheetAt(0); // Get Your Sheet.
+
+            int index = 0;
+            for (Row row : sheet) { // For each Row.
+                if (index == 3)
+                    break;
+
+                if (index == 0) {
+                    index++;
+                    continue;
+                }
+                Cell cell = row.getCell(0); // Get the Cell at the Index / Column you want.
+                System.out.println("--" + cell.getStringCellValue());
+                String[] date = cell.getStringCellValue().split("-");
+                HICRI_YEAR = date[0];
+                HICRI_DAY = date[1];
+                HICRI_MONTH_CODE = HicriMonthID.va.date[2];
+                callWebsite();
+
+                index++;
+            }
+
+            // Closing the workbook
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
